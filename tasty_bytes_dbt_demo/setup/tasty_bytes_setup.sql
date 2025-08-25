398bc1fbe84142dc185d69e4e558cc269d15884b
aladeeb <ahmed.aladeeb@merckgroup.com>
2025-08-25 22:01:58 +0000

    TEST

--- tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
+++ tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
@@ -221,4 +221,4 @@ COPY INTO tasty_bytes_dbt_db.raw.order_detail
 FROM @tasty_bytes_dbt_db.public.s3load/raw_pos/order_detail/;
 
 -- setup completion note
-SELECT 'tasty_bytes_dbt_db setup is now complete' AS note;
+SELECT 'tasty_bytes_dbt_db setup is now complete' AS note; 
